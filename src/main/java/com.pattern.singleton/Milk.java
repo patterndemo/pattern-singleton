87168c50f2ee91785258aa041243d6b9758a421a
renxiaoshuai <shuai_0303@163.com>
2019-04-16 23:22:47 +0800

    例子

---
+++ src/main/java/com.pattern.singleton/Milk.java
@@ -0,0 +1,14 @@
+package com.pattern.factory;/*
+ @author 天赋吉运-xxx
+ @DESCRIPTION 
+ @create 2019/4/15
+*/
+
+public interface Milk {
+
+    /**
+     * 获取一个标准的产品
+     * @return
+     */
+    public String getName();
+}
